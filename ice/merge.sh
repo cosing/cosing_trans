b4c69266fecf29ad50d3e2d19bad3b0df9b5329e
cosing <cocosing@protonmail.com>
2024-06-08 23:00:24 +0800

    渲染 增加merge.sh脚本

---
+++ ice/merge.sh
@@ -0,0 +1,1 @@
+cat cover.md Foreword.md chapter_1.md chapter_2.md chapter_3.md chapter_4.md chapter_5.md chapter_6.md chapter_7.md chapter_8.md chapter_9.md chapter_10.md chapter_11.md chapter_12.md chapter_13.md chapter_14.md chapter_15.md Afterword.md>merge.md
